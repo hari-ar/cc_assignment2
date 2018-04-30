5a1f1038b54ba4bf21f19388b6605ca103d691b2
larhari <aahuyarakshaka.harilakshminarasimhan@student.griffith.ie>
2018-04-30 21:25:44 +0530

    Final

--- delete.py
+++ delete.py
@@ -58,39 +58,18 @@ class DeleteBooking(webapp2.RequestHandler):
     def post(self):
         self.response.headers['Content-Type'] = 'text/html'
         user = users.get_current_user()
-        error_message = ''
-        room_list = ''
-        # User Authentication.
-        if user:
-            main_header = 'Rooms Information'
-            login_logout = 'Logout'
-            login_logout_url = users.create_logout_url(self.request.uri)
-            room_list = RoomModel.query().fetch()
-            user_given_room_name = self.request.get("room_name")
-            user_room_key = ndb.Key('RoomModel', user_given_room_name)
-            room_ndb_object = user_room_key.get()
-            new_booking_id = self.request.get("bookingId")
-            print("Booking id")
-            print(new_booking_id)
-            existing_bookings = room_ndb_object.booking
-            for each_booking in existing_bookings:  # Steps to identify selected booking
-                if each_booking.bookingId == new_booking_id:
-                    room_ndb_object.booking.remove(each_booking)  # Pass the matched booking object
-                    room_ndb_object.put()  # Persist back to ndb
-                    break
+        main_header = 'Rooms Information'
+        login_logout = 'Logout'
+        login_logout_url = users.create_logout_url(self.request.uri)
+        room_list = RoomModel.query().fetch()
+        user_given_room_name = self.request.get("room_name")
+        user_room_key = ndb.Key('RoomModel', user_given_room_name)
+        room_ndb_object = user_room_key.get()
+        new_booking_id = self.request.get("bookingId")
+        for index, each_booking in enumerate(room_ndb_object.booking):
+            if each_booking.bookingId == new_booking_id:
+                del room_ndb_object.booking[index]
+                room_ndb_object.put()
+                break
+        self.redirect('/')
 
-        else:
-            main_header = 'Please Login to Access This Page..!!'
-            login_logout = 'Login'
-            login_logout_url = users.create_login_url(self.request.uri)
-        template_values = {
-            'main_header': main_header,
-            'login_logout': login_logout,
-            'login_logout_url': login_logout_url,
-            'user': user,
-            'room_list': room_list,
-            'error_message': error_message
-        }
-
-        template = JINJA_ENVIRONMENT.get_template('main.html')
-        self.response.write(template.render(template_values))
